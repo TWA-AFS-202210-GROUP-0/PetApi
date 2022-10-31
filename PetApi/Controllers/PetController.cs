939508f970d09b84e51b3d9bc0f92749136ca03b
binrui <bmu2@slb.com>
2022-10-31 17:05:16 +0800

    feat: add the delete and modify price by name

---
+++ PetApi/Controllers/PetController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetApi;
+using System.Collections.Generic;
+
+namespace PetApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class PetController : Controller
+    {
+        private static List<Pet> pets = new List<Pet>();
+        [HttpPost("addNewPet")]
+        public Pet AddNewPet(Pet pet)
+        {
+            pets.Add(pet);
+            return pet;
+        }
+
+        [HttpGet("getAllPets")]
+        public List<Pet> GetAllPet()
+        {
+            return pets;
+        }
+
+        [HttpGet("findPetByName")]
+        public Pet FindGetByName([FromQuery] string name)
+        {
+            return pets.Find(pet => pet.Name == name);
+        }
+
+        [HttpDelete("deletePetByName")]
+        public List<Pet> DeleteByName([FromQuery] string name)
+        {
+            var pet = pets.Find(pet => pet.Name == name);
+            pets.Remove(pet);
+            return pets;
+        }
+
+        [HttpDelete("deleteAllPets")]
+        public void DeleteAllPets()
+        {
+            pets.Clear();
+        }
+
+        [HttpPut("modifyPriceoOfPet")]
+        public Pet ModifyPriceByName([FromQuery] string name, [FromBody] Pet pet)
+        {
+            var oldPet = pets.Find(pet => pet.Name == name);
+            oldPet.Price = pet.Price;
+            return oldPet;
+        }
+    }
+}
---
+++ PetApi/Model/Pet.cs
@@ -0,0 +1,37 @@
+﻿namespace PetApi
+{
+    public class Pet
+    {
+        private string name;
+        private string type;
+        private string color;
+        private int price;
+
+        public Pet(string name, string type, string color, int price)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Color = color;
+            this.Price = price;
+        }
+
+        public Pet()
+        {
+        }
+
+        public int Price { get => price; set => price = value; }
+        public string Color { get => color; set => color = value; }
+        public string Type { get => type; set => type = value; }
+        public string Name { get => name; set => name = value; }
+
+        public override bool Equals(object? obj)
+        {
+            var pet = obj as Pet;
+            return pet != null &&
+                Name.Equals(pet.Name) &&
+                Type.Equals(pet.Type) &&
+                Price.Equals(pet.Price) &&
+                Color.Equals(pet.Color);    
+        }
+    }
+}
