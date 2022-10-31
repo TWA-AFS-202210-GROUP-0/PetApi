b41b49b0fd1dfef10b467f7daee86c8e846e31b7
Xiaotian WU <XWu32@slb.com>
2022-10-31 14:42:57 +0800

    test: find by name

---
+++ PetApi/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PetApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class PetController
+    {
+        private static List<Pet> Pets { get; set; } = new List<Pet>();
+
+        [HttpPost("addNewPet")]
+        public Pet AddNewPet(Pet pet)
+        {
+            Pets.Add(pet);
+            return pet;
+        }
+
+        [HttpGet("getAllPets")]
+        public List<Pet> GetAllPets()
+        {
+            return Pets;
+        }
+
+        [HttpGet("getPetByName")]
+        public Pet FindPetByName([FromQuery] string name)
+        {
+            return Pets.Find(p => p.Name.Equals(name, System.StringComparison.Ordinal));
+        }
+    }
+}
--- PetApiTest/ControllerTest/HelloControllerTest.cs
+++ PetApiTest/ControllerTest/HelloControllerTest.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using PetApi;
+using System.Collections.Generic;
+using System.Net.Http.Json;
 using Xunit;
 
 namespace PetApiTest.ControllerTest;
@@ -6,7 +11,53 @@ namespace PetApiTest.ControllerTest;
 public class HelloControllerTest
 {
     [Fact]
-    public async void Should_return_hello_world()
+    public async void Should_add_new_pet()
     {
+        var application = new WebApplicationFactory<Program>();
+        var client = application.CreateClient();
+        var pet = new Pet(name: "Kitty", type: "cat", color: "white", price: 100);
+
+        var response = await client.PostAsJsonAsync("api/addNewPet", pet);
+        string res = response.Content.ReadAsStringAsync().Result;
+        var resPet = JsonConvert.DeserializeObject<Pet>(res);
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(resPet, pet);
+    }
+
+    [Fact]
+    public async void Should_get_all_pets()
+    {
+        // given
+        var application = new WebApplicationFactory<Program>();
+        var client = application.CreateClient();
+        var pet = new Pet(name: "Kitty", type: "cat", color: "white", price: 100);
+
+        await client.PostAsJsonAsync("api/addNewPet", pet);
+
+        // when
+        var response = await client.GetAsync("api/getAllPets");
+        string responseBody = response.Content.ReadAsStringAsync().Result;
+        var allPets = JsonConvert.DeserializeObject<List<Pet>>(responseBody);
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(allPets[0], pet);
+    }
+
+    [Fact]
+    public async void Should_get_pet_by_name()
+    {
+        // given
+        var application = new WebApplicationFactory<Program>();
+        var client = application.CreateClient();
+        var pet = new Pet(name: "Kitty", type: "cat", color: "white", price: 100);
+
+        await client.PostAsJsonAsync("api/addNewPet", pet);
+        await client.PostAsJsonAsync("api/addNewPet", new Pet(name: "ab", type: "cat", color: "white", price: 100));
+
+        // when
+        var response = await client.GetAsync("api/getPetByName?name=Kitty");
+        string responseBody = response.Content.ReadAsStringAsync().Result;
+        var resPet = JsonConvert.DeserializeObject<Pet>(responseBody);
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(resPet, pet);
     }
 }
